99e379aab4e7e7d4686aee5f73a9edd3893e592c
Aayush <aayush.singh@hbwsl.com>
2025-07-04 08:56:00 +0530

    Add modular css to reduce page size

--- header.php
+++ header.php
@@ -44,6 +44,7 @@ if ( ! function_exists( 'check_is_responsive_addons_greater' ) ) {
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="profile" href="http://gmpg.org/xfn/11"/>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>"/>
+		
 		<?php wp_head(); ?>
 	</head>
 
